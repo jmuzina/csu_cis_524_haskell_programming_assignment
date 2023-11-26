16c462468c95152805266090fe5ddd0e32133946
jmuzina <jmuzina@jmuzina.io>
2023-11-26 10:43:11 -0500

    #1 factorial

--- JulieMuzina_Assignment1.hs
+++ JulieMuzina_Assignment1.hs
@@ -3,3 +3,16 @@
     CIS 524
     Haskell Programming Assignment
 -}
+
+{-
+    #1. Write a Haskell function factorial that calculates the factorial of a given integer n. 
+    Ensure the function handles edge cases like 0 and negative numbers.
+    @param n {Integer} number to calculate factorial for
+    @returns {Integer} n!
+-}
+factorial :: Integer -> Integer
+factorial n
+    -- Guard clause to handle edge cases first
+    | n < 0 = 0
+    | n <= 1 = 1
+    | otherwise = n * factorial (n - 1)
